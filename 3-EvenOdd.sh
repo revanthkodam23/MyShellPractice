6fdd983f3ae555861d7aca81900b089d3069ca3c
revanthkodam23 <revanth.kodam@outlook.com>
2025-09-23 10:45:23 +0530

    Conditions Practive

--- 3-EvenOdd.sh
+++ 3-EvenOdd.sh
@@ -3,7 +3,7 @@
 echo "please Enter the number"
 read Number
 
-if [ $(($Number%2)) -eq 0]; then
+if [ $(($Number % 2)) -eq 0 ]; then
     echo "given number $Number is Even Number"
 else
     echo "given number $Number is Odd Number" 
